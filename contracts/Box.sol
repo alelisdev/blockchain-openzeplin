43a8b1e542cd12d3c3a83889263eb8026a6b0dfd
Steven Pollard <topcoder728@mail.ru>
2022-02-17 16:32:04 -0800

    feat: add openzeplin

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -1,13 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
+// Import Auth from the access-control subdirectory
+import "./access-control/Auth.sol";
+
 contract Box {
     uint256 private _value;
+
+    Auth private _auth;
 
     // Emitted when the stored value changes
     event ValueChanged(uint256 value);
 
+    constructor() {
+        _auth = new Auth(msg.sender);
+    }
+
     // Stores a new value in the contract
     function store(uint256 value) public {
+        // Require that the caller is registered as an administrator in Auth
+        require(_auth.isAdministrator(msg.sender), "Unauthorized");
+
         _value = value;
         emit ValueChanged(value);
     }
